d0cef6dd898514697b86cb2c7b75583a4165ac8f
dsoto06 <diegosr06@hotmail.com>
2021-02-18 02:30:40 +0000

    Entrega Name Generator

---
+++ app.js
@@ -0,0 +1,15 @@
+let namegenerator = function() {
+
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+
+let random1 = Math.floor(Math.random() * pronoun.length);
+let random2 = Math.floor(Math.random() * adj.length);
+let random3 = Math.floor(Math.random() * noun.length);
+
+
+return pronoun[random1] + adj[random2] + noun[random3] + ".com"
+};
+
+console.log(namegenerator());
