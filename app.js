471bbe8f9f8f86aa1ca45a9824754ddd2885756b
dsoto06 <diegosr06@hotmail.com>
2021-03-11 02:25:00 +0000

    entrega final

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-let namegenerator = function() {
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let domain = [".com", ".es", ".net", ".us", ".io"]
 
-let pronoun = ['the','our'];
-let adj = ['great', 'big' ];
-let noun = ['jogger','racoon'];
+for (let p = 0; p < pronoun.length; p++){
 
-let random1 = Math.floor(Math.random() * pronoun.length);
-let random2 = Math.floor(Math.random() * adj.length);
-let random3 = Math.floor(Math.random() * noun.length);
+    for(let a = 0; a < adj.length; a++){
 
+        for(let n = 0; n < noun.length; n++){
 
-return pronoun[random1] + adj[random2] + noun[random3] + ".com"
-};
+            for(let d = 0; d < domain.length; d++){
 
-console.log(namegenerator());
+                console.log(pronoun[p]+adj[a]+noun[n]+domain[d]);
+            }
+        }
+    }
+}
 
 // Write [node app.js] on the console to see the result
